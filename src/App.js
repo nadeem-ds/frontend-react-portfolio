9b7b37057bae26069ad16d0e6938b449c108237a
Nadeem <anwarnadeem2010@gmail.com>
2022-08-02 14:00:54 +0530

    Created Folder Structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
-
+import { About, Footer, Header, Skills, Testimonial, Work } from "./container";
+import { Navbar } from "./components";
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Nadeem ANwar personal portfolio </h1>
+      <Navbar />
+      <About />
+      <Footer />
+      <Header />
+      <Skills />
+      <Testimonial />
+      <Work />
     </div>
   );
 }
